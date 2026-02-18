1b0a332d433dc4f5933a80eeabcf09d646c0b57c
zulqarnain-git <aapka-email@example.com>
2026-02-18 21:30:23 +0500

    Final professional website upload

---
+++ wp-content/plugins/smooth-back-to-top-button/assets/js/smooth-back-to-top-button62ea.js
@@ -0,0 +1,28 @@
+(function ($) {
+    "use strict";
+
+    jQuery(window).on('load', function () {
+
+        var progressPath = document.querySelector('.progress-wrap path');
+        var pathLength = progressPath.getTotalLength();
+
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+        progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+        progressPath.style.strokeDashoffset = pathLength;
+        progressPath.getBoundingClientRect();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+
+        var updateProgress = function () {
+            var scroll = jQuery(window).scrollTop();
+            var height = jQuery(document).height() - jQuery(window).height();
+            var progress = pathLength - (scroll * pathLength / height);
+            progressPath.style.strokeDashoffset = progress;
+        };
+
+        updateProgress();
+
+        jQuery(window).scroll(updateProgress);
+
+    });
+
+})(jQuery);
